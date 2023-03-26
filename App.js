b7f7d696a298550ef74095b224a1bf6dcf2efd19
physicistgaurav <gauravrijal03@gmail.com>
2023-03-26 10:02:14 +0545

    newchangetest

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello there</Text>
+      <Text>Hello there =. wassup dude</Text>
       <StatusBar style="auto" />
     </View>
   );
