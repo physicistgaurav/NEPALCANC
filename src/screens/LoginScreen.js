c9360d61ccfa17cc70b9638cbfddac363de50617
physicistgaurav <gauravrijal03@gmail.com>
2023-03-26 11:41:18 +0545

    loginuidone

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -18,9 +18,37 @@ const LoginScreen = ({ navigation }) => {
   const [password, setPassword] = React.useState("");
 
   return (
-    <View style={styles.header}>
-      <Text style={styles.headertext1}>Nepal Can</Text>
-      <Text style={styles.headertext2}>Login</Text>
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.headertext1}>Nepal Can Code</Text>
+        <Text style={styles.headertext2}>Login</Text>
+      </View>
+      <View style={styles.logincontainer}>
+        <Text style={styles.textcontainerlogin}>Log in</Text>
+        <Divider style={styles.divider}></Divider>
+        <Text style={styles.loginlabel}>Email</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Enter your email"
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+          />
+        </View>
+        <Text style={styles.loginlabel}>Password</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Enter your password."
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Log in</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -38,7 +66,7 @@ const styles = StyleSheet.create({
   },
   headertext1: {
     color: "white",
-    margin: 10,
+    margin: 5,
     fontSize: 20,
   },
   headertext2: {
@@ -46,5 +74,53 @@ const styles = StyleSheet.create({
     margin: 10,
     fontSize: 20,
     paddingLeft: 150,
+  },
+  logincontainer: {
+    backgroundColor: "#e9ecef",
+    margin: 50,
+    width: 300,
+    height: 320,
+    borderRadius: 4,
+  },
+  textcontainerlogin: {
+    color: "red",
+    paddingLeft: 10,
+    margin: 10,
+    fontSize: 20,
+  },
+  divider: {
+    margin: 5,
+  },
+  loginlabel: {
+    margin: 15,
+    fontSize: 16,
+  },
+
+  inputView: {
+    backgroundColor: "white",
+    borderRadius: 3,
+    width: "90%",
+    height: 45,
+    // marginBottom: 20,
+    marginLeft: 15,
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    alignSelf: "baseline",
+  },
+  button: {
+    backgroundColor: "#057dfe",
+    padding: 10,
+    borderRadius: 4,
+    width: "30%",
+    marginTop: 15,
+    marginLeft: 15,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
   },
 });
